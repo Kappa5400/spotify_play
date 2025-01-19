53cb94dc056b2f3052e876deddbbc98de0295247
Colin <kappa6708@gmail.com>
2025-01-19 17:04:30 -0600

    Updating

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, redirect, url_for, request
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
@@ -14,6 +14,30 @@ client_secret = "32484a0911b94b0db3a35b2b1e8a788e"
 redirect_uri = "http://localhost:3000"
 scope = "playlist-read-private"
 
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id=client_id,
+    client_secret = client_secret,
+    redirect_uri = redirect_uri,
+    scope = scope,
+    cache_handler=cache_handler,
+    show_dialog=True
+)
+
+sp = Spotify(auth_manager=sp_oauth)
+
+@app.route('/')
+def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_playlists'))
+
+
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
 
 
 if __name__ == '__main__':
